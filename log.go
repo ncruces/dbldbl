c202b9eb762aefd73c88cafb4358155a65ec5cc6
Nuno Cruces <ncruces@users.noreply.github.com>
2023-08-21 19:04:58 +0100

    Log.

--- log.go
+++ log.go
@@ -6,43 +6,31 @@ func Log(n Number) Number {
 		return NaN()
 	case n.y == 0:
 		return Inf(-1)
+	case n.y == 1:
+		return Number{y: n.x}
 	case !isFinite(n.y):
 		return n
-	case n == Number{1, 0}:
-		return Number{}
 	}
 
-	negate := false
-	if n.y < 1 {
+	negate := true
+	if n.y > 1 {
 		n = Div(Float(1), n)
-		negate = true
+		negate = false
 	}
 
 	var shift int8
-	for n.y < 8 {
+	for n.y > 0x1p-54 {
 		n = Sqr(n)
 		shift--
 	}
 
-	n = Shift(log(n), shift)
+	// log(1/x) = π / 2⋅AGM(1, 4⋅x)
+	n = Div(Pi, agm(Float(1), Shift(n, 2)))
+	n = Shift(n, shift-1)
 	if negate {
 		n = Neg(n)
 	}
 	return n
-}
-
-func log(n Number) Number {
-	// Sasaki and Kanada's formula, with q=q⁴
-	// to avoid the ∜q̅ term in the θ₂(q) expansion:
-	// https://maths-people.anu.edu.au/~brent/pd/RNC7t.pdf
-	q := Div(Float(1), n)
-	q4 := Sqr(Sqr(q))
-	q8 := Sqr(q4)
-	q16 := Sqr(q8)
-	q24 := Mul(q16, q8)
-	th2 := Shift(Mul(q, AddFloat(Add(q8, q24), 1)), 1)
-	th3 := AddFloat(Shift(Add(q4, q16), 1), 1)
-	return Div(Shift(Pi, -2), agm(Sqr(th2), Sqr(th3)))
 }
 
 func agm(a, g Number) Number {
